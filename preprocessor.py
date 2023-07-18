3fc1010ddb0b1c47ff80f27c9c51fecc5dac7598
Etizaz Shah <139033592+etizazshah@users.noreply.github.com>
2023-07-17 23:20:28 -0400

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,40 +1,41 @@
-# Importing relevant packages
-
-import pickle
-from nltk.stem.snowball import EnglishStemmer
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from gensim.parsing.preprocessing import remove_stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk import word_tokenize
-import nltk  # Import NLTK (Natural Language Toolkit) for natural language processing tasks
 
-
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 def preprocess_text(text):
-    # Instantiating our lemmatizer
-    lemma = EnglishStemmer()
+    # Instantiate the lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # Handle contractions and expand abbreviations (you can add more)
+    text = text.replace("can't", "can not")
+    text = text.replace("don't", "do not")
+
     # Remove URLs
-    text = ' '.join(word for word in text.split() if not word.startswith('http'))
-    text = ' '.join(word for word in text.split() if not word.startswith('www'))
+    text = re.sub(r'http\S+|www\S+', '', text)
+
+    # Remove mentions (@username)
+    text = re.sub(r'@\w+', '', text)
 
     # Remove special characters and punctuation
     text = ''.join(char for char in text if char.isalnum() or char.isspace())
 
-    # Remove mentions (@username)
-    text = ' '.join(word for word in text.split() if not word.startswith('@'))
+    # Lowercase the text
+    text = text.lower()
 
-    # Remove hashtags (#technology)
-    text = ' '.join(word[1:] if word.startswith('#') else word for word in text.split())
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    text = remove_stopwords(text)
 
-    # Removing stopwords
-    ## NB: Remember to convert the text into thier lowercase form so that for example "I" will be exactly the same as "i"
-    text = remove_stopwords(text.lower())
+    # Tokenization and lemmatization
+    tokens = word_tokenize(text)
+    text = ' '.join(lemmatizer.lemmatize(token) for token in tokens)
 
-    # Tokenization
-    text = word_tokenize(text)
-
-    #lemmatization
-    text = ' '.join([lemma.stem(word) for word in text])
-
-    # Splitting the text back
-    text = word_tokenize(text)
     return text
